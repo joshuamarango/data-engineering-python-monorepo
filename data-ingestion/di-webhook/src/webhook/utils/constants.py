382b6be8b5587572847a6aaf24d23c7da506fff0
Joshua Marango <42380352+joshuamarango@users.noreply.github.com>
2025-03-22 16:48:43 +0000

    feat/di-webhook/xero-data-processing (#6)
    * ✨ feat: refactor Xero webhook processing and add Pydantic model
    
    * ✨ feat: enhance Xero webhook processing with Pydantic models
    
    * ✨ feat: refactor Xero webhook handling and add payload models

--- data-ingestion/di-webhook/src/webhook/utils/constants.py
+++ data-ingestion/di-webhook/src/webhook/utils/constants.py
@@ -2,6 +2,6 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-class WebhookSettings(BaseSettings):
+class EnvironmentVariables(BaseSettings):
     lambda_run_id: str = Field(default="", env="LAMBDA_RUN_ID")
-    xero_webhook_key: str = Field(..., env="XERO_WEBHOOK_KEY")
+    ssm_param_xero: str = Field(..., env="SSM_PARAM_XERO")
